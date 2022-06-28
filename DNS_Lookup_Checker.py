65dfd6b43991498b53fd576bec7ec85196b13e22
SeyitCenan <106025852+SeyitCenan@users.noreply.github.com>
2022-06-29 01:29:55 +0300

    Update DNS_Lookup_Checker.py

--- DNS_Lookup_Checker.py
+++ DNS_Lookup_Checker.py
@@ -7,7 +7,7 @@ slug_f=input("Yapmak istediginiz tarama türünü yazınız Ex:a-record-lookup. 
 domain_a=input("Tarama yapmak istediginiz domaini giriniz Ex: securityforeveryone.com.   ")
 headers = {'Content-Type': 'application/json','accept':'application/json'}
 data_f = { "slug":slug_f, "asset": domain_a,'token':'IQFgQd5ohak6GcAfpqKOuq9fk5rGfhOtG2jz3jSet0tiH9zi8AdkLKvhGfNXmy5H7aczRgx4qlJEiF7K_VWtkQ'}
-r = requests.post(domain_f, data=simplejson.dumps(data_f), headers=headers)
+r = requests.post(domain_f, data=json.dumps(data_f), headers=headers)
 json_object = json.loads(r.text)
 anl_s=json_object["value"]["analysis_slug"]
 job_s=json_object["value"]["job_slugs"][0]
